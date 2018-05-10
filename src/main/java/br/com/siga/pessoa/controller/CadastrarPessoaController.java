303bc0d35a7c1a96c979a2dd3bbe8e343ae5a8ad
GAfelipe <felipe.gabriel.mariz@hotmail.com>
2018-05-09 21:36:45 -0300

    Sistema de login, cadastro e listagem de Aluno CRUD #399

---
+++ src/main/java/br/com/siga/pessoa/controller/CadastrarPessoaController.java
@@ -0,0 +1,54 @@
+package br.com.siga.pessoa.controller;
+
+import java.text.ParseException;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+ 
+import br.com.siga.model.PessoaModel;
+import br.com.siga.repository.PessoaRepository;
+import br.com.siga.usuario.controller.UsuarioController;
+import br.com.siga.uteis.Uteis;
+ 
+@Named(value="cadastrarPessoaController")
+@RequestScoped
+public class CadastrarPessoaController {
+ 
+	@Inject
+	PessoaModel pessoaModel;
+ 
+	@Inject
+	UsuarioController usuarioController;
+ 
+	@Inject
+	PessoaRepository pessoaRepository;
+ 
+ 
+	public PessoaModel getPessoaModel() {
+		return pessoaModel;
+	}
+ 
+	public void setPessoaModel(PessoaModel pessoaModel) {
+		this.pessoaModel = pessoaModel;
+	}
+ 
+	/**
+	 *SALVA UM NOVO REGISTRO VIA INPUT 
+	 * @throws ParseException 
+	 */
+	public void SalvarNovaPessoa() throws ParseException{
+ 
+		pessoaModel.setUsuarioModel(this.usuarioController.GetUsuarioSession());
+ 
+		//INFORMANDO QUE O CADASTRO FOI VIA INPUT
+ 
+		pessoaRepository.SalvarNovoRegistro(this.pessoaModel);
+ 
+		this.pessoaModel = null;
+ 
+		Uteis.MensagemInfo("Registro cadastrado com sucesso");
+ 
+	}
+ 
+}
---
+++ src/main/java/br/com/siga/repository/PessoaRepository.java
@@ -0,0 +1,114 @@
+package br.com.siga.repository;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.siga.model.PessoaModel;
+import br.com.siga.model.UsuarioModel;
+import br.com.siga.repository.entity.PessoaEntity;
+import br.com.siga.repository.entity.UsuarioEntity;
+import br.com.siga.uteis.Uteis;
+ 
+public class PessoaRepository {
+ 
+	@Inject
+	PessoaEntity pessoaEntity;
+ 
+	EntityManager entityManager;
+ 
+	/***
+	 * MÉTODO RESPONSÁVEL POR SALVAR UMA NOVA PESSOA
+	 * @param pessoaModel
+	 * @throws ParseException 
+	 */
+	public void SalvarNovoRegistro(PessoaModel pessoaModel) throws ParseException{
+ 
+		entityManager =  Uteis.JpaEntityManager();
+		
+		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+		Date dataFormatada = formato.parse(pessoaModel.getData());
+		
+		pessoaEntity = new PessoaEntity();
+		pessoaEntity.setCodigo(pessoaModel.getCodigo());
+		pessoaEntity.setNome(pessoaModel.getNome());
+		pessoaEntity.setSexo(pessoaModel.getSexo());
+		pessoaEntity.setData(dataFormatada);
+		pessoaEntity.setIdade(pessoaModel.getIdade());
+		pessoaEntity.setPai(pessoaModel.getPai());
+		pessoaEntity.setMae(pessoaModel.getMae());
+		pessoaEntity.setTelefone(pessoaModel.getTelefone());
+		pessoaEntity.setEndereco(pessoaModel.getEndereco());
+		pessoaEntity.setDataCad(LocalDateTime.now());
+ 
+ 
+		UsuarioEntity usuarioEntity = entityManager.find(UsuarioEntity.class, pessoaModel.getUsuarioModel().getCodigo()); 
+ 
+		pessoaEntity.setUsuarioEntity(usuarioEntity);
+ 
+		entityManager.persist(pessoaEntity);
+ 
+	}
+	/***
+	 * MÉTODO PARA CONSULTAR A PESSOA
+	 * @return
+	 */
+	public List<PessoaModel> GetPessoas(){
+ 
+		List<PessoaModel> pessoasModel = new ArrayList<PessoaModel>();
+ 
+		entityManager =  Uteis.JpaEntityManager();
+ 
+		Query query = entityManager.createNamedQuery("PessoaEntity.findAll");
+ 
+		@SuppressWarnings("unchecked")
+		Collection<PessoaEntity> pessoasEntity = (Collection<PessoaEntity>)query.getResultList();
+ 
+		PessoaModel pessoaModel = null;
+ 
+		for (PessoaEntity pessoaEntity : pessoasEntity) {
+			
+			SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+			String dataFormatada = formatador.format(pessoaEntity.getData());
+ 
+			pessoaModel = new PessoaModel();
+			pessoaModel.setCodigo(pessoaEntity.getCodigo());
+			pessoaModel.setNome(pessoaEntity.getNome());
+			pessoaModel.setData(dataFormatada);
+			pessoaModel.setIdade(pessoaEntity.getIdade());
+			pessoaModel.setPai(pessoaEntity.getPai());
+			pessoaModel.setMae(pessoaEntity.getMae());
+			pessoaModel.setTelefone(pessoaEntity.getTelefone());
+			pessoaModel.setEndereco(pessoaEntity.getEndereco());
+			pessoaModel.setDataCad(pessoaEntity.getDataCad());
+			
+ 
+ 
+			if(pessoaEntity.getSexo().equals("M"))
+				pessoaModel.setSexo("Masculino");
+			else
+				pessoaModel.setSexo("Feminino");
+ 
+			UsuarioEntity usuarioEntity =  pessoaEntity.getUsuarioEntity();			
+ 
+			UsuarioModel usuarioModel = new UsuarioModel();
+			usuarioModel.setUsuario(usuarioEntity.getUsuario());
+ 
+			pessoaModel.setUsuarioModel(usuarioModel);
+ 
+			pessoasModel.add(pessoaModel);
+		}
+ 
+		return pessoasModel;
+ 
+	}
+}
+
