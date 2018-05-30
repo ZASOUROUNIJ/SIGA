fcb1cc62e60e05893a26999cb63ac238b04027ed
Jorge Júnio <jorge_junio_silva@hotmail.com>
2018-05-29 23:48:21 -0300

    Teste rodando e Gerando Site

--- src/test/java/br/com/siga/service/test/InsertAlunoTest.java
+++ src/test/java/br/com/siga/service/test/InsertAlunoTest.java
@@ -16,6 +16,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import br.com.siga.model.PessoaModel;
+import br.com.siga.model.UsuarioModel;
 import br.com.siga.repository.PessoaRepository;
 
 public class InsertAlunoTest {
@@ -29,6 +30,17 @@ public class InsertAlunoTest {
 	@Before
 	public void setUp() throws Exception {
 		dao = new PessoaRepository();
+		/*
+		pessoaModel = new PessoaModel();
+		pessoaModel.setNome("Antonio Carlos Florentino De Souza Junior");
+		pessoaModel.setSexo("M");
+		pessoaModel.setData("16/05/2018");
+		pessoaModel.setIdade(22);
+		pessoaModel.setPai("Antonio Carlos Florentino De Souza");
+		pessoaModel.setMae("Maria Assunção Pereira");
+		pessoaModel.setTelefone("(84)996869885");
+		pessoaModel.setEndereco("Rua: Major Ajax");*/
+		
 		pessoaModel = new PessoaModel();
 		pessoaModel.setNome("Antonio Carlos Florentino De Souza Junior");
 		pessoaModel.setSexo("M");
@@ -38,22 +50,41 @@ public class InsertAlunoTest {
 		pessoaModel.setMae("Maria Assunção Pereira");
 		pessoaModel.setTelefone("(84)996869885");
 		pessoaModel.setEndereco("Rua: Major Ajax");
+
+		UsuarioModel usuarioEntity = new UsuarioModel();
+		usuarioEntity.setCodigo("1");
+		usuarioEntity.setSenha("123456");
+		usuarioEntity.setUsuario("admin");
+
+		pessoaModel.setUsuarioModel(usuarioEntity);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		//dao.ExcluirRegistro(pessoaModel.getCodigo());
-		dao = null;
+		 try {
+			 dao.ExcluirRegistro(pessoaModel.getCodigo());
+	        } catch (Exception e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+		
+		//dao = null;
 	}
 	
 	@Test
 	public void testSalvarNovoRegistro() throws Exception {
-
+		List<PessoaModel> pessoas = dao.GetPessoas();
+		int valueOld = pessoas.size();
+		
 		dao.SalvarNovoRegistro(pessoaModel);
 		
-		List<PessoaModel> pessoas = dao.GetPessoas();
+		pessoas = dao.GetPessoas();
+		int valueNew = pessoas.size();
 		
-		assertTrue("T01", pessoas.contains(pessoaModel));
+		//assertTrue("T01", pessoas.contains(pessoaModel));
+		assertEquals(valueOld+1, valueNew);
+		
+		
 		
 	}
 	/*
