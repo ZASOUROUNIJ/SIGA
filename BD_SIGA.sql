c8ab1cf9f307673a031189d243545c10a00b4875
ZASOUROUNIJ <antoniocfsjunior@gmail.com>
2018-06-26 14:28:31 -0300

    att do Banco de Dados #509

--- BD_SIGA.sql
+++ BD_SIGA.sql
@@ -4,16 +4,17 @@ use db_estudo_java;
 
 CREATE TABLE db_estudo_java.tb_usuario(
  
-	id_usuario INT AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT 'CÓDIGO DO USUÁRIO',
+	id_usuario INT AUTO_INCREMENT COMMENT 'CÓDIGO DO USUÁRIO',
 	ds_login   VARCHAR(30) NOT NULL COMMENT 'LOGIN DO USUÁRIO PARA ACESSO AO SISTEMA',
-	ds_senha   VARCHAR(30) NOT NULL COMMENT 'SENHA DO USUÁRIO PARA ACESSO AO SISTEMA'   	
+	ds_senha   VARCHAR(30) NOT NULL COMMENT 'SENHA DO USUÁRIO PARA ACESSO AO SISTEMA',
+   	PRIMARY KEY (id_usuario)
  
 );
 
 
 CREATE TABLE db_estudo_java.tb_aluno(
  
-    matricula INT AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT 'CÓDIGO DA PESSOA',
+    matricula INT AUTO_INCREMENT COMMENT 'CÓDIGO DA PESSOA',
     nome VARCHAR(70) NOT NULL COMMENT 'NOME DA PESSOA',
     sexo CHAR(1) NOT NULL COMMENT 'INFORMAR M OU F',
     dataNasc DATE NOT NULL COMMENT 'DATA DE NASCIMENTO',
@@ -22,52 +23,47 @@ CREATE TABLE db_estudo_java.tb_aluno(
     nomeMae VARCHAR(70) NOT NULL COMMENT 'NOME DA MÃe',
     telefone VARCHAR(20) NOT NULL COMMENT 'TELEFONE PRA CONTATO',
     endereco VARCHAR(200) NOT NULL COMMENT 'DESCRIÇÃO DO ENDEREÇO',
-    dt_cadastro DATETIME NOT NULL COMMENT 'DATA DE CADASTRO DO REGISTRO'
+    dt_cadastro DATETIME NOT NULL COMMENT 'DATA DE CADASTRO DO REGISTRO',
+    PRIMARY KEY (matricula)
 );
 
 CREATE TABLE db_estudo_java.tb_professor(
  
-    matricula INT AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT 'CÓDIGO DA PESSOA',
+    matricula INT AUTO_INCREMENT COMMENT 'CÓDIGO DA PESSOA',
     nome VARCHAR(70)  NOT NULL COMMENT 'NOME DA PESSOA',
     dataNasc DATE NOT NULL COMMENT 'DATA DE NASCIMENTO',
     telefone VARCHAR(20) NOT NULL COMMENT 'TELEFONE PRA CONTATO',
     endereco VARCHAR(200) NOT NULL COMMENT 'DESCRIÇÃO DO ENDEREÇO',
-    dt_cadastro DATETIME NOT NULL COMMENT 'DATA DE CADASTRO DO REGISTRO'
+    dt_cadastro DATETIME NOT NULL COMMENT 'DATA DE CADASTRO DO REGISTRO',
+        PRIMARY KEY (matricula)
 );
 
 CREATE TABLE db_estudo_java.tb_turma(
-	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT 'CÓDIGO DA TURMA',
+	id INT AUTO_INCREMENT COMMENT 'CÓDIGO DA TURMA',
     nivel VARCHAR(32) NOT NULL COMMENT 'NIVEL DA TURMA',
     nome VARCHAR(32) NOT NULL COMMENT 'NOME DA TURMA',
     turno VARCHAR(16) NOT NULL COMMENT 'TURNO',
     professor INT COMMENT 'PROFESSOR RESPONSAVEL PELA TURMA',
+    PRIMARY KEY (id),
     FOREIGN KEY (professor) REFERENCES db_estudo_java.tb_professor(matricula)
 );
 
 CREATE TABLE db_estudo_java.tb_turma_aluno(
-	id INT NOT NULL COMMENT 'CÓDIGO DA TURMA',
-    	matricula INT NOT NULL COMMENT 'CÓDIGO DA PESSOA',
-    	ano INT NOT NULL,
-    	PRIMARY KEY (id, matricula),
-    	FOREIGN KEY (id) REFERENCES tb_turma(id),
-    	FOREIGN KEY (matricula) REFERENCES tb_aluno(matricula)   
+	id INT COMMENT 'CÓDIGO DA TURMA',
+	matricula INT NOT NULL COMMENT 'CÓDIGO DA PESSOA',
+	ano INT NOT NULL,
+	PRIMARY KEY (id, matricula),
+	FOREIGN KEY (id) REFERENCES tb_turma(id),
+	FOREIGN KEY (matricula) REFERENCES tb_aluno(matricula)   
 );
 
+INSERT INTO db_estudo_java.tb_usuario (ds_login,ds_senha) VALUES('admin','admin');
+/*
+INSERT INTO db_estudo_java.tb_aluno (nome,sexo,dataNasc,idade,nomePai,nomeMae,telefone,endereco,dt_cadastro) VALUES ('João','M','2010-06-06',8,'José Maria','Maria josé','996265878','rua francisca silva',current_timestamp());
+INSERT INTO db_estudo_java.tb_professor(nome,dataNasc,telefone,endereco,dt_cadastro) VALUES('Gabriel Diniz','1990-01-01','999696969','Rua elizabeth Farias',current_timestamp());
+update db_estudo_java.tb_professor set id_usuario_cadastro = 1;
+INSERT INTO db_estudo_java.tb_turma(nivel,nome,turno,professor) VALUES ('1','A','M',1);
+update db_estudo_java.tb_turma set id_usuario_cadastro = 1;
+INSERT INTO db_estudo_java.tb_turma_aluno (id, matricula, ano) VALUES (1,1,2018);
 
-
-INSERT INTO db_estudo_java.tb_usuario (ds_login,ds_senha) VALUES('admin','123456');
-
--- ALTER TABLE db_estudo_java.tb_turma ADD FOREIGN KEY (professor) REFERENCES db_estudo_java.tb_professor(matricula);
-
-
-
--- Update db_estudo_java.tb_pessoa (nm_pessoa, fl_sexo, dt_cadastro, ds_email,
- -- ds_endereco, fl_origemCadastro, id_usuario_cadastro) values ();
- 
- select * from tb_aluno;
-  select * from tb_turma_aluno;
-UPDATE `db_estudo_java`.`tb_aluno` SET `nome`='Arthur gorgo' WHERE `matricula`='33';
-
- update tb_aluno set nome='Arthur Gorg' where matricula=33;
- 
- insert into tb_turma_aluno(id, matricula, ano) values (1, 1, 2018);
+*/
